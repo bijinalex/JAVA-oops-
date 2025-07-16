cad041d9b97a0d75a80d74712c4d090386c49a17
BIJIN ALEX <bijinalex2028@cs.sjcetpalai.ac.in>
2025-07-16 10:41:53 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -7,9 +7,14 @@ public class Factorial{
 		Scanner sc =new Scanner(System.in);
         System.out.println("Enter the Number:");
         int a = sc.nextInt();
+	if(a==0){
+		f=1;
+	}
+	else{
         for(i=1;i<=a;i++) {
         	f*=i;
-        }
+	 }
+	}
         System.out.println("The factorial of "+a+" is :"+f);
 	  }
    }
