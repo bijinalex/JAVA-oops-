12ca0b1ee65b8c0d66da65d9056cb548fa7e8220
BIJIN ALEX <bijinalex2028@cs.sjcetpalai.ac.in>
2025-07-30 13:48:20 +0530

    Update BookDetails.java

--- BookDetails.java
+++ BookDetails.java
@@ -19,11 +19,13 @@ package java_lab_projects;
 
 	
  public void displayDetails(){
-	System.out.println("Title:"+title);
-	System.out.println("Author:"+author);
-	System.out.println("Price:"+ price);
-	System.out.println("Quantity:"+ quantity);
-	System.out.println("TotalValue:"+ totalvalue);
+	System.out.println("Title:"+this.title);
+	System.out.println("Author:"+ this.author);
+	System.out.println("Price:"+ this.price);
+	System.out.println("Quantity:"+ this.quantity);
+	
+	TotalValue();
+	System.out.println("Total Value:" + this.totalvalue);
 	}
   }
 	
@@ -31,14 +33,12 @@ public class BookDetails {
 	public static void main(String[] args) {
 		System.out.println("Book 1 Details:\n");
 		Book b1=new Book("The Eye","Chris.H",390,1);
-		b1.TotalValue();
 		b1.displayDetails();
 		
 		System.out.println("\n");
 		
 		System.out.println("Book 2 Details:\n");
 		Book b2=new Book("Marcus","Evan.S.T",500,2);
-		b2.TotalValue();
 		b2.displayDetails();
 	}
 }
